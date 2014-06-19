153669f1c074679457f5fbd880c9031f5e0e11e5
iapilgrim <iapilgrim@gmail.com>
2014-06-19 06:52:34 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/me.rb
@@ -0,0 +1,4 @@
+set[:about_me][:first_name] = "Pilgrim"
+default[:about_me][:favorite][:food] = "pizza"
+default[:about_me][:favorite][:drink] = "water"
+default[:about_me][:favorite][:hobby] = "running"
